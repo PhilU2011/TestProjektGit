3e8021a04240ae9f557744c2ec3a37b4ba864de8
phili <phili@DESKTOP-OA6NLER.mshome.net>
2022-01-14 08:08:48 +0100

    Erstes Commit

---
+++ TestProjekt/src/testStart/Start.java
@@ -0,0 +1,15 @@
+package testStart;
+
+public class Start {
+
+	public static void main (String args []) {
+		
+		Entity neueEntity  =new Entity(1,2);
+		
+		System.out.println("Start des Projekts");
+		System.out.println(neueEntity.getA());
+		System.out.println(neueEntity.getB());
+	}
+	
+	
+}
