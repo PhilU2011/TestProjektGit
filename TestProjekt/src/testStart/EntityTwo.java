ce2cef3287b08227d66d38ae9331fbeed960a172
phili <phili@DESKTOP-OA6NLER.mshome.net>
2022-01-14 08:58:46 +0100

    Commit Drei

--- TestProjekt/src/testStart/EntityTwo.java
+++ TestProjekt/src/testStart/EntityTwo.java
@@ -5,9 +5,12 @@ public class EntityTwo {
 	private int x;
 	private int y;
 	
-	public EntityTwo(int x, int y) {
+	public EntityTwo(int x, int z) {
 		this.x=x;
-		this.y=y;
+		this.y=z;
 	}
 	
+	public void startX(int a) {
+		this.x=a;
+	}
 }
